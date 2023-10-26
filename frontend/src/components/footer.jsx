5e1da54230a6d1a509980435d1c5c69654bab9d2
DLamarG <demondg8@gmail.com>
2023-10-26 16:51:59 -0500

    version 2.0

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -8,7 +8,7 @@ function Footer(){
           <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
             Bougie Belly
           </a>
-          <span className="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+          <span className="mb-3 mb-md-0 text-muted">© 2023 Recipes, Inc</span>
         </div>
 
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
